ffabdfa345d73c2093177678890536ae5886c1bd
eaint yemarn <yemarn.510@gmail.com>
2020-10-20 21:45:44 +0630

    remove if from drawing

--- index.ts
+++ index.ts
@@ -34,9 +34,7 @@ let inputs: Input[] = [];
 function remove(tile: Tile) {
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
-      if (map[y][x] === tile) {
-        map[y][x] = Tile.AIR;
-      }
+      (map[y][x] === tile) ? map[y][x] = Tile.AIR : {}; 
     }
   }
 }
@@ -121,19 +119,12 @@ function draw() {
   // Draw map
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
-      if (map[y][x] === Tile.FLUX)
-        g.fillStyle = "#ccffcc";
-      else if (map[y][x] === Tile.UNBREAKABLE)
-        g.fillStyle = "#999999";
-      else if (map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
-        g.fillStyle = "#0000cc";
-      else if (map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
-        g.fillStyle = "#8b4513";
-      else if (map[y][x] === Tile.KEY1 || map[y][x] === Tile.LOCK1)
-        g.fillStyle = "#ffcc00";
-      else if (map[y][x] === Tile.KEY2 || map[y][x] === Tile.LOCK2)
-        g.fillStyle = "#00ccff";
-
+      g.fillStyle = (map[y][x] === Tile.FLUX) ? "#ccffcc" :
+                    (map[y][x] === Tile.UNBREAKABLE) ? "#999999" :
+                    (map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE) ? "#0000cc" :
+                    (map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX) ? "#8b4513" :
+                    (map[y][x] === Tile.KEY1 || map[y][x] === Tile.LOCK1) ? "#ffcc00" :
+                    (map[y][x] === Tile.KEY2 || map[y][x] === Tile.LOCK2) ? "#00ccff" : null;
       if (map[y][x] !== Tile.AIR)
         g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
     }
